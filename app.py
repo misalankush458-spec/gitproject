04eed9ea7eddaaebd65afef04cb5ed0cd97ee6e3
misalankush458-spec <misal.ankush458@gmail.com>
2026-06-13 13:08:59 +0530

    change

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 def add(a,b):
     return a+b
-=======
-def add(a,b,c):
-    return a+b+c
->>>>>>> 9d4b9e34085f5191f964f8b358802d0fa9a45086
 
-def sub(a,b,c)
+def sub(a,b)
     return a-b-c
 
 
